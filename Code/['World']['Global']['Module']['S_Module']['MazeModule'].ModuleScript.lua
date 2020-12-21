ddb21c6ffc0f9fe830e0a43f3b4b6649458b5704
Yuancheng Zhang <old.far.hot@gmail.com>
2020-12-21 18:11:03 +0800

    Update ['World']['Global']['Module']['S_Module']['MazeModule'].ModuleScript.lua
    更新迷宫：使用对象池

--- Code/['World']['Global']['Module']['S_Module']['MazeModule'].ModuleScript.lua
+++ Code/['World']['Global']['Module']['S_Module']['MazeModule'].ModuleScript.lua
@@ -13,6 +13,8 @@ local CELL_SIDE = 2
 local WALL_ROOT_CENTER = Vector3(100, 1.5, 0)
 
 local WALL_ROOT_NODE = world.MiniGames.Game_03_Maze.Walls
+local WALL_POOL_ROOT = world.WallPool
+local WALL_POOL_POS = Vector3.Down * 100
 local WALL_POS_OFFSET = 1
 local CELL_POS_OFFSET = CELL_SIDE
 local WALL_DICT = {}
@@ -50,22 +52,92 @@ local r, c = 1, 1
 -- # The history is the stack of visited locations
 local history = Stack:New()
 
+-- 对象池
+local pool, poolDone = {}, false
+
 --! 打印事件日志, true:开启打印
 local showLog, PrintMazeData = false
 
+--! 初始化
+
+-- 初始化
 function Maze:Init()
-    print('Maze:Init')
-    InitData()
-    MazeDataInit()
+    print('[Maze] Init()')
+    invoke(InitWallPool)
+end
+
+-- 初始化对象池
+function InitWallPool()
+    if WALL_POOL_ROOT == nil then
+        world:CreateObject('FolderObject', 'WallPool', world)
+    end
+    WALL_POOL_ROOT = world.WallPool
+
+    -- 总共需要多少面墙
+    -- 外墙数 = NUM_ROWS * 2 + NUM_COLS * 2
+    -- 内墙数 = (NUM_ROWS - 1) * (NUM_COLS - 1) * 2
+    -- 出入口 = -2
+    local wallNeeded = NUM_ROWS * 2 + NUM_COLS * 2 + (NUM_ROWS - 1) * (NUM_COLS - 1) * 2 - 2
+    print('[Maze] InitWallPool() 需要墙数', wallNeeded)
+    local rot = EulerDegree(0, 0, 0)
+    local wallName
+    for i = 1, wallNeeded do
+        wallName = string.format('%s_%04d', WALL_ARCH, i)
+        objWall = world:CreateInstance(WALL_ARCH, wallName, WALL_POOL_ROOT, WALL_POOL_POS, rot)
+        pool[objWall] = true
+        if i % 5 == 0 then
+            wait()
+        end
+    end
+    poolDone = true
+    print('[Maze] InitWallPool() done')
+end
+
+function SpawnWall(_pos, _rot)
+    for obj, available in pairs(pool) do
+        if available then
+            pool[obj] = false
+            obj.Position = _pos
+            obj.Rotation = _rot
+            return obj
+        end
+    end
+    error('[Maze] 墙体数量不够')
+end
+
+function DespawnWall(_obj)
+    assert(_obj and not _obj:IsNull(), '[Maze] DespawnWall(_obj) _obj不能为空')
+    assert(pool[_obj] ~= nil, '[Maze] DespawnWall(_obj) _obj不在对象池中')
+    assert(pool[_obj] == false, '[Maze] DespawnWall(_obj) _obj对象池状态错误')
+    _obj.Position = WALL_POOL_POS
+    pool[_obj] = true
+end
+
+function DespaceWalls()
+    for obj, _ in pairs(pool) do
+        obj.Position = WALL_POOL_POS
+        pool[obj] = true
+    end
+end
+
+--! 迷宫生成
+
+-- 迷宫重置
+function MazeReset()
+    if not poolDone then
+        print('[Maze] 对象池初始化未完成，MazeReset() 不能执行')
+        return
+    end
+    print('[Maze] MazeReset() 迷宫重置')
+    MazeDataReset()
+    MazeWallReset()
     MazeDataGen()
     PrintMazeData()
-    invoke(MazeWallGen)
+    MazeWallGen()
 end
 
-function InitData()
-end
-
-function MazeDataInit()
+-- 迷宫数据重置
+function MazeDataReset()
     for row = 1, NUM_ROWS do
         M[row] = {}
         for col = 1, NUM_COLS do
@@ -74,6 +146,12 @@ function MazeDataInit()
     end
 end
 
+-- 迷宫墙壁重置
+function MazeWallReset()
+    DespaceWalls()
+end
+
+-- 迷宫数据生成
 function MazeDataGen()
     r, c = 1, 1
     -- cell data
@@ -140,26 +218,39 @@ function MazeDataGen()
     M[NUM_ROWS][NUM_COLS][RIGHT] = 1
 end
 
+-- 迷宫墙体生成
 function MazeWallGen()
     local wallName
     local objWall
     local cell, pos, rot
-    for row = 1, NUM_ROWS do
-        for col = 1, NUM_COLS do
-            cell = M[row][col]
-            for dir = 1, 4 do
+    for dir = 1, 4 do
+        for row = 1, NUM_ROWS do
+            for col = 1, NUM_COLS do
+                cell = M[row][col]
                 if cell[dir] == 0 then
-                    wallName = string.format('%s_%s_%s_%s', WALL_ARCH, row, col, WALL_DICT[dir].dir)
                     pos = Vector3(col * CELL_POS_OFFSET, 0, row * -CELL_POS_OFFSET) + WALL_DICT[dir].pos
                     rot = WALL_DICT[dir].rot
-                    objWall = world:CreateInstance(WALL_ARCH, wallName, WALL_ROOT_NODE, pos, rot)
+                    objWall = SpawnWall(pos, rot)
                 end
             end
-            wait()
         end
     end
 end
 
+--! Event handlers 事件处理
+
+-- 进入小游戏事件
+-- @param _player 玩家
+-- @param _gameId 游戏ID
+function Maze:EnterMiniGameEventHandler(_player, _gameId)
+    print('[Maze] EnterMiniGameEventHandler', _player, _gameId)
+    if _player and _gameId == Const.GameEnum.MAZE then
+        MazeReset()
+    end
+end
+
+--! Aux 辅助功能
+-- 日志打印
 PrintMazeData = showLog and function()
         for row = 1, NUM_ROWS do
             for col = 1, NUM_COLS do
