f8a977830b7ffeb4169e5db6a79bdd1ad7ea5f01
Yuancheng Zhang <old.far.hot@gmail.com>
2020-12-23 19:10:38 +0800

    Update ['World']['Global']['Module']['S_Module']['MazeModule'].ModuleScript.lua
    迷宫添加寻路

--- Code/['World']['Global']['Module']['S_Module']['MazeModule'].ModuleScript.lua
+++ Code/['World']['Global']['Module']['S_Module']['MazeModule'].ModuleScript.lua
@@ -9,6 +9,9 @@ local Maze, this = ModuleUtil.New('Maze', ServerBase)
 -- 迷宫尺寸
 local NUM_ROWS, NUM_COLS = 7, 10
 
+-- 迷宫Hierachy根节点
+local MAZE_ROOT = world.MiniGames.Game_03_Maze
+
 -- 迷宫中心位置
 local MAZE_CENTER_POS = Vector3(100, 1.5, 0)
 local MAZE_CENTER_ROT = EulerDegree(0, 0, 0)
@@ -20,12 +23,15 @@ local LEFT, UP, RIGHT, DOWN, VISITED = 1, 2, 3, 4, 5
 local ENTRANCE = math.floor((NUM_ROWS + 1) * .5)
 local EXIT = math.ceil((NUM_ROWS + 1) * .5)
 
+-- 入口出口对象
+local entrace, exit
+
 --! 常量配置: Floor 迷宫地板相关
 
 -- 迷宫地板厚度
 local MAZE_FLOOR_THICKNESS = 0.2
 -- 迷宫地板颜色
-local MAZE_FLOOR_COLOR = Color(255, 255, 255, 100)
+local MAZE_FLOOR_COLOR = Color(0xFF, 0xFF, 0xFF, 100)
 -- 迷宫地板Obj，根据迷宫中心和尺寸生成
 local floor
 
@@ -49,7 +55,7 @@ local WALL_LENGTH = 2 -- 对应Size.X
 local WALL_THICKNESS = 0.2 -- 对应Size.Z
 
 -- 墙壁对象池Hierachy根节点
-local WALL_POOL_ROOT = world.MiniGames.Game_03_Maze
+local WALL_POOL_ROOT
 -- 墙壁对象池隐藏默认位置
 local WALL_POOL_POS = Vector3.Down * 100
 
@@ -61,22 +67,26 @@ local WALL_DICT = {}
 WALL_DICT[LEFT] = {
     pos = CELL_LEFT_UP_POS + Vector3.Left * WALL_POS_OFFSET + Vector3.Up * WALL_HEIGHT * .5,
     rot = EulerDegree(0, -90, 0),
-    dir = 'L'
+    dir = 'L',
+    symbol = '←'
 }
 WALL_DICT[UP] = {
     pos = CELL_LEFT_UP_POS + Vector3.Forward * WALL_POS_OFFSET + Vector3.Up * WALL_HEIGHT * .5,
     rot = EulerDegree(0, 0, 0),
-    dir = 'U'
+    dir = 'U',
+    symbol = '↑'
 }
 WALL_DICT[RIGHT] = {
     pos = CELL_LEFT_UP_POS + Vector3.Right * WALL_POS_OFFSET + Vector3.Up * WALL_HEIGHT * .5,
     rot = EulerDegree(0, 90, 0),
-    dir = 'R'
+    dir = 'R',
+    symbol = '→'
 }
 WALL_DICT[DOWN] = {
     pos = CELL_LEFT_UP_POS + Vector3.Back * WALL_POS_OFFSET + Vector3.Up * WALL_HEIGHT * .5,
     rot = EulerDegree(0, 180, 0),
-    dir = 'D'
+    dir = 'D',
+    symbol = '↓'
 }
 
 --! 迷宫生成数据信息
@@ -99,29 +109,59 @@ local history = Stack:New()
 -- 对象池，对象池生成完毕
 local pool, poolDone = {}, false
 
+--! 其他数据
+
+-- 玩家进入Maze前的数据，退出游戏后需要接恢复
+local pTrans = {}
+
+-- 寻路节点
+local path = {}
+
 --! 打印事件日志, true:开启打印
-local showLog, PrintMazeData = false
+local showLog, PrintMazeData, PrintNodePath = false
 
 --! 初始化
 
 -- 初始化
 function Maze:Init()
     print('[Maze] Init()')
+    InitMazeWallSpace()
     InitMazeFloor()
+    InitMazeEntranceAndExit()
     invoke(InitWallPool)
-    invoke(MazeReset, 2)
+
+    --* TEST ONLY
+    -- invoke(MazeReset, 5)
 end
 
--- 初始化迷宫地板
+-- 初始化迷宫墙壁空间
+function InitMazeWallSpace()
+    WALL_POOL_ROOT = world:CreateObject('Cube', 'Maze_Wall_Space', MAZE_ROOT, MAZE_CENTER_POS, MAZE_CENTER_ROT)
+    WALL_POOL_ROOT.Color = Color(0xFF, 0xFF, 0xFF, 0)
+end
+
+-- 初始化迷宫地板，与Maze_Wall_Space大小一致
 function InitMazeFloor()
-    floor = world:CreateObject('Cube', 'Maze_Floor', WALL_POOL_ROOT, MAZE_CENTER_POS, MAZE_CENTER_ROT)
+    floor = world:CreateObject('Cube', 'Maze_Floor', MAZE_ROOT, MAZE_CENTER_POS, MAZE_CENTER_ROT)
     floor.Color = MAZE_FLOOR_COLOR
+    -- floor:SetActive(false)
+end
+
+-- 初始化迷宫入口出口
+function InitMazeEntranceAndExit()
+    entrace = world:CreateObject('Sphere', 'Entrance', floor)
+    exit = world:CreateObject('Sphere', 'Exit', floor)
+    entrace.Size = Vector3.One * 0.3
+    exit.Size = Vector3.One * 0.3
+    entrace.Color = Color(0x00, 0xFF, 0x00, 0xFF)
+    exit.Color = Color(0xFF, 0x00, 0x00, 0xFF)
+    entrace:SetActive(false)
+    exit:SetActive(false)
 end
 
 -- 初始化对象池
 function InitWallPool()
-    WALL_POOL_ROOT = floor
-
+    assert(WALL_POOL_ROOT and not WALL_POOL_ROOT:IsNull(), '[Maze] WALL_POOL_ROOT 为空')
     -- 总共需要多少面墙
     -- 外墙数 = NUM_ROWS * 2 + NUM_COLS * 2
     -- 内墙数 = (NUM_ROWS - 1) * (NUM_COLS - 1) * 2
@@ -179,22 +219,40 @@ end
 -- 迷宫重置
 function MazeReset()
     if not poolDone then
-        print('[Maze] 对象池初始化未完成，MazeReset() 不能执行')
+        error('[Maze] 对象池初始化未完成，MazeReset() 不能执行')
         return
     end
     print('[Maze] MazeReset() 迷宫重置')
+    -- reset
     MazeFloorReset()
+    MazeEntraceAndExitReset()
     MazeDataReset()
     MazeWallsReset()
+    CheckPointReset()
+    -- data
     MazeDataGen()
+    FindNodePath()
+    -- print log
     PrintMazeData()
+    PrintNodePath()
+    -- gen objs
     MazeWallsGen()
+    CheckPointGen()
 end
 
+-- 重置地板
 function MazeFloorReset()
     local rowlen = NUM_ROWS * CELL_SIDE
     local collen = NUM_COLS * CELL_SIDE
     floor.Size = Vector3(collen, MAZE_FLOOR_THICKNESS, rowlen)
+    -- floor:SetActive(true)
+end
+
+function MazeEntraceAndExitReset()
+    entrace.LocalPosition = Vector3(1, 0, -ENTRANCE) * CELL_POS_OFFSET + CELL_LEFT_UP_POS
+    exit.LocalPosition = Vector3(NUM_COLS, 0, -EXIT) * CELL_POS_OFFSET + CELL_LEFT_UP_POS
+    entrace:SetActive(true)
+    exit:SetActive(true)
 end
 
 -- 迷宫数据重置
@@ -212,13 +270,21 @@ function MazeWallsReset()
     DespaceWalls()
 end
 
+-- 迷宫检查点重置
+function CheckPointReset()
+    --TODO: 重置
+end
+
 -- 迷宫数据生成
 function MazeDataGen()
-    r, c = 1, 1
+    -- start point
+    local r, c = ENTRANCE, 1
+    -- destination point
+    local tr, tc = EXIT, NUM_COLS
     -- cell data
     local cell = {r, c}
     -- unvisited cell direction
-    local check
+    local dirs
     -- move direction
     local dir
 
@@ -227,43 +293,41 @@ function MazeDataGen()
 
     while not history:IsEmpty() do
         -- designate this location as visited
+        -- VISITED, 0:未访问 ,1:已访问
         M[r][c][VISITED] = 1
-        -- check if the adjacent cells are valid for moving to
-        check = {}
+        -- dirs if the adjacent cells are valid for moving to
+        dirs = {}
         if c > 1 and M[r][c - 1][VISITED] == 0 then
-            table.insert(check, LEFT)
+            table.insert(dirs, LEFT)
         end
         if r > 1 and M[r - 1][c][VISITED] == 0 then
-            table.insert(check, UP)
+            table.insert(dirs, UP)
         end
         if c < NUM_COLS and M[r][c + 1][VISITED] == 0 then
-            table.insert(check, RIGHT)
+            table.insert(dirs, RIGHT)
         end
         if r < NUM_ROWS and M[r + 1][c][VISITED] == 0 then
-            table.insert(check, DOWN)
+            table.insert(dirs, DOWN)
         end
 
         -- If there is a valid cell to move to.
         -- Mark the walls between cells as open if we move
-        if #check > 0 then
+        if #dirs > 0 then
             history:Push({r, c})
-            dir = table.shuffle(check)[1]
+            dir = table.shuffle(dirs)[1]
             if dir == LEFT then
                 M[r][c][LEFT] = 1
                 c = c - 1
                 M[r][c][RIGHT] = 1
-            end
-            if dir == UP then
+            elseif dir == UP then
                 M[r][c][UP] = 1
                 r = r - 1
                 M[r][c][DOWN] = 1
-            end
-            if dir == RIGHT then
+            elseif dir == RIGHT then
                 M[r][c][RIGHT] = 1
                 c = c + 1
                 M[r][c][LEFT] = 1
-            end
-            if dir == DOWN then
+            elseif dir == DOWN then
                 M[r][c][DOWN] = 1
                 r = r + 1
                 M[r][c][UP] = 1
@@ -289,13 +353,100 @@ function MazeWallsGen()
             for col = 1, NUM_COLS do
                 cell = M[row][col]
                 if cell[dir] == 0 then
-                    pos = Vector3(col * CELL_POS_OFFSET, 0, row * -CELL_POS_OFFSET) + WALL_DICT[dir].pos
+                    pos = Vector3(col, 0, -row) * CELL_POS_OFFSET + WALL_DICT[dir].pos
                     rot = WALL_DICT[dir].rot
                     objWall = SpawnWall(pos, rot)
                 end
             end
         end
     end
+end
+
+-- 找出迷宫路径
+function FindNodePath()
+    -- start point
+    local r, c = ENTRANCE, 1
+    -- target point
+    local tr, tc = EXIT, NUM_COLS
+
+    history:Clear()
+    path = {}
+
+    -- found destination point
+    local found = false
+    -- 用于寻路
+    local inDir, outDir = nil, RIGHT
+    local idx, node = 1
+    -- avaliable directions
+    local dirs
+
+    while not found do
+        inDir = outDir
+
+        -- 找到方向
+        dirs = {}
+        if M[r][c][LEFT] == 1 and inDir ~= RIGHT then
+            table.insert(dirs, LEFT)
+        end
+        if M[r][c][UP] == 1 and inDir ~= DOWN then
+            table.insert(dirs, UP)
+        end
+        if M[r][c][RIGHT] == 1 and inDir ~= LEFT then
+            table.insert(dirs, RIGHT)
+        end
+        if M[r][c][DOWN] == 1 and inDir ~= UP then
+            table.insert(dirs, DOWN)
+        end
+
+        -- 根据可用方向判定情况
+        if #dirs > 0 then
+            -- 继续走
+            outDir = dirs[1]
+            if dirs[2] then
+                history:Push({r, c, inDir, dirs[2], idx})
+            end
+            if dirs[3] then
+                history:Push({r, c, inDir, dirs[3], idx})
+            end
+        else
+            -- 走到尽头
+            node = history:Pop()
+            r, c, inDir, outDir, idx = table.unpack(node)
+        end
+
+        -- 记录节点信息
+        path[idx] = {r, c, inDir, outDir}
+
+        -- 移动到下一个节点
+        if outDir == LEFT then
+            c = c - 1
+        elseif outDir == UP then
+            r = r - 1
+        elseif outDir == RIGHT then
+            c = c + 1
+        elseif outDir == DOWN then
+            r = r + 1
+        end
+        idx = idx + 1
+        found = (r == tr and c == tc)
+    end
+end
+
+-- 生成检查点
+function CheckPointGen()
+    --TODO: 生成检查点
+end
+
+--! 玩家数据的缓存与读取
+
+-- 玩家进入迷宫前，数据缓存
+function CachePlayerTrans(_player)
+    pTrans.pos = _player.Position
+    pTrans.rot = _player.Rotation
+end
+
+-- 玩家离开迷宫后，读取数据
+function LoadPlayerTrans(_player)
 end
 
 --! Event handlers 事件处理
@@ -307,17 +458,28 @@ function Maze:EnterMiniGameEventHandler(_player, _gameId)
     print('[Maze] EnterMiniGameEventHandler', _player, _gameId)
     if _player and _gameId == Const.GameEnum.MAZE then
         MazeReset()
+        CachePlayerTrans(_player)
+        NetUtil.Fire_C('ClientMazeEvent', _player, Const.MazeEventEnum.JOIN, entrace.Position)
     end
 end
 
 --! Aux 辅助功能
--- 日志打印
+-- 打印迷宫数据
 PrintMazeData = showLog and function()
         for row = 1, NUM_ROWS do
             for col = 1, NUM_COLS do
                 print(table.dump(M[row][col]))
             end
             print('============================')
+        end
+    end or function()
+    end
+
+-- 打印寻路结果
+PrintNodePath = showLog and function()
+        print('打印寻路结果')
+        for k, n in pairs(path) do
+            print(string.format('[%02d] %s (%s, %s) %s', k, WALL_DICT[n[3]].symbol, n[1], n[2], WALL_DICT[n[4]].symbol))
         end
     end or function()
     end
