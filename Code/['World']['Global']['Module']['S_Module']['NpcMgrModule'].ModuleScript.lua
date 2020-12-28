00416933f6e5fe153f46cb93144f1f5eba264162
Yuancheng Zhang <zhangyuancheng@lilith.com>
2020-12-28 17:42:43 +0800

    Update ['World']['Global']['Module']['S_Module']['NpcMgrModule'].ModuleScript.lua
    格式化代码

--- Code/['World']['Global']['Module']['S_Module']['NpcMgrModule'].ModuleScript.lua
+++ Code/['World']['Global']['Module']['S_Module']['NpcMgrModule'].ModuleScript.lua
@@ -30,20 +30,31 @@ function SpawnNpcs()
     for _, npc in pairs(NpcInfo) do
         local npcObj = world:CreateInstance(npc.Model, npc.Name, npcFolder, npc.SpawnPos, npc.SpawnRot)
         local npcInfo = npc
-		if true then --这里是根据表判断这个NPC是否是可战斗的NPC
-			invoke(function()
-				wait(1)
-				local _healthVal = world:CreateObject('IntValueObject','HealthVal',npcObj)
-				local _attackVal = world:CreateObject('IntValueObject','AttackVal',npcObj)
-				local _monsterVal = world:CreateObject('ObjRefValueObject','MonsterVal',npcObj)
-				world:CreateObject('IntValueObject','BattleVal',npcObj)
-				_monsterVal.Value = world:CreateInstance('Monster','Monster'..npc.Name,world,npcObj.Position - npcObj.Forward*2)
-				InitMonster(npcObj,_monsterVal.Value)
-			end)
-		end
+        if true then --这里是根据表判断这个NPC是否是可战斗的NPC
+            invoke(
+                function()
+                    wait(1)
+                    local _healthVal = world:CreateObject('IntValueObject', 'HealthVal', npcObj)
+                    local _attackVal = world:CreateObject('IntValueObject', 'AttackVal', npcObj)
+                    local _monsterVal = world:CreateObject('ObjRefValueObject', 'MonsterVal', npcObj)
+                    world:CreateObject('IntValueObject', 'BattleVal', npcObj)
+                    _monsterVal.Value =
+                        world:CreateInstance(
+                        'Monster',
+                        'Monster' .. npc.Name,
+                        world,
+                        npcObj.Position - npcObj.Forward * 2
+                    )
+                    InitMonster(npcObj, _monsterVal.Value)
+                end
+            )
+        end
         npcObj.CollisionArea.OnCollisionBegin:Connect(
             function(_hitObj)
-                if not _hitObj or _hitObj.ClassName ~= 'PlayerInstance' or not _hitObj.Avatar or _hitObj.Avatar.ClassName ~= 'PlayerAvatarInstance' then
+                if
+                    not _hitObj or _hitObj.ClassName ~= 'PlayerInstance' or not _hitObj.Avatar or
+                        _hitObj.Avatar.ClassName ~= 'PlayerAvatarInstance'
+                 then
                     return
                 end
                 print(npcObj.Name, _hitObj)
@@ -52,30 +63,47 @@ function SpawnNpcs()
         )
         npcObj.CollisionArea.OnCollisionEnd:Connect(
             function(_hitObj)
-                if not _hitObj or _hitObj.ClassName ~= 'PlayerInstance' or not _hitObj.Avatar or _hitObj.Avatar.ClassName ~= 'PlayerAvatarInstance' then
-					return
+                if
+                    not _hitObj or _hitObj.ClassName ~= 'PlayerInstance' or not _hitObj.Avatar or
+                        _hitObj.Avatar.ClassName ~= 'PlayerAvatarInstance'
+                 then
+                    return
                 end
                 NetUtil.Fire_C('TouchNpcEvent', _hitObj, nil, nil)
             end
         )
-		table.insert(npcObjs, npcObj)
+        table.insert(npcObjs, npcObj)
     end
 end
 
-function InitMonster(_npcobj,_monster)
-	invoke(function() 
-		local _moveTime = 2
-		while true do
-			if _monster and _monster.Cube then
-				local Tweener = Tween:TweenProperty(_monster.Cube,{LocalPosition = Vector3(0,2.5,0)},_moveTime,Enum.EaseCurve.Linear)
-				Tweener:Play()
-				wait(_moveTime)
-				Tweener = Tween:TweenProperty(_monster.Cube,{LocalPosition = Vector3(0,2,0)},_moveTime,Enum.EaseCurve.Linear)
-				Tweener:Play()
-				wait(_moveTime)
-			end
-		end
-	end)
+function InitMonster(_npcobj, _monster)
+    invoke(
+        function()
+            local _moveTime = 2
+            while true do
+                if _monster and _monster.Cube then
+                    local Tweener =
+                        Tween:TweenProperty(
+                        _monster.Cube,
+                        {LocalPosition = Vector3(0, 2.5, 0)},
+                        _moveTime,
+                        Enum.EaseCurve.Linear
+                    )
+                    Tweener:Play()
+                    wait(_moveTime)
+                    Tweener =
+                        Tween:TweenProperty(
+                        _monster.Cube,
+                        {LocalPosition = Vector3(0, 2, 0)},
+                        _moveTime,
+                        Enum.EaseCurve.Linear
+                    )
+                    Tweener:Play()
+                    wait(_moveTime)
+                end
+            end
+        end
+    )
 end
 
 return NpcMgr
