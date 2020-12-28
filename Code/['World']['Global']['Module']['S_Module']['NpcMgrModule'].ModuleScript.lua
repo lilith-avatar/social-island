30295aae2ad75dec11493e4eb17e92ba52504029
Yuancheng Zhang <zhangyuancheng@lilith.com>
2020-12-28 18:39:52 +0800

    Update ['World']['Global']['Module']['S_Module']['NpcMgrModule'].ModuleScript.lua
    拆分npc生成和怪物生成逻辑

--- Code/['World']['Global']['Module']['S_Module']['NpcMgrModule'].ModuleScript.lua
+++ Code/['World']['Global']['Module']['S_Module']['NpcMgrModule'].ModuleScript.lua
@@ -14,7 +14,6 @@ local npcObjs = {}
 function NpcMgr:Init()
     CreateNpcFolder()
     SpawnNpcs()
-    print(table.dump(npcObjs))
 end
 
 --- 生成节点：world.NPC
@@ -29,78 +28,95 @@ end
 function SpawnNpcs()
     for _, npc in pairs(NpcInfo) do
         local npcObj = world:CreateInstance(npc.Model, npc.Name, npcFolder, npc.SpawnPos, npc.SpawnRot)
-        local npcInfo = npc
-        if true then --这里是根据表判断这个NPC是否是可战斗的NPC
-            invoke(
-                function()
-                    wait(1)
-                    local _healthVal = world:CreateObject('IntValueObject', 'HealthVal', npcObj)
-                    local _attackVal = world:CreateObject('IntValueObject', 'AttackVal', npcObj)
-                    local _monsterVal = world:CreateObject('ObjRefValueObject', 'MonsterVal', npcObj)
-                    world:CreateObject('IntValueObject', 'BattleVal', npcObj)
-                    _monsterVal.Value =
-                        world:CreateInstance(
-                        'Monster',
-                        'Monster' .. npc.Name,
-                        world,
-                        npcObj.Position - npcObj.Forward * 2
-                    )
-                    InitMonster(npcObj, _monsterVal.Value)
-                end
-            )
-        end
+        local id = world:CreateObject('IntValueObject', 'ID', npcObj)
+        id.Value = npc.ID
+
+        -- 生成宠物
+        SpawnMonster(npcObj, npc)
+
+        local npcInfo = npc -- 用于闭包
         npcObj.CollisionArea.OnCollisionBegin:Connect(
             function(_hitObj)
-                if
-                    not _hitObj or _hitObj.ClassName ~= 'PlayerInstance' or not _hitObj.Avatar or
-                        _hitObj.Avatar.ClassName ~= 'PlayerAvatarInstance'
-                 then
-                    return
+                if CheckHitObjIsPlayer(_hitObj) then
+                    NetUtil.Fire_C('TouchNpcEvent', _hitObj, npcInfo.ID, npcObj)
                 end
-                print(npcObj.Name, _hitObj)
-                NetUtil.Fire_C('TouchNpcEvent', _hitObj, npcInfo.ID, npcObj)
             end
         )
         npcObj.CollisionArea.OnCollisionEnd:Connect(
             function(_hitObj)
-                if
-                    not _hitObj or _hitObj.ClassName ~= 'PlayerInstance' or not _hitObj.Avatar or
-                        _hitObj.Avatar.ClassName ~= 'PlayerAvatarInstance'
-                 then
-                    return
+                if CheckHitObjIsPlayer(_hitObj) then
+                    NetUtil.Fire_C('TouchNpcEvent', _hitObj, nil, nil)
                 end
-                NetUtil.Fire_C('TouchNpcEvent', _hitObj, nil, nil)
             end
         )
         table.insert(npcObjs, npcObj)
     end
 end
 
-function InitMonster(_npcobj, _monster)
+-- 检查碰撞对象是否为NPC
+function CheckHitObjIsPlayer(_hitObj)
+    return _hitObj and _hitObj.ClassName == 'PlayerInstance' and _hitObj.Avatar and
+        _hitObj.Avatar.ClassName == 'PlayerAvatarInstance'
+end
+
+-- 创建NPC的宠物
+function SpawnMonster(_npcObj, _npcInfo)
+    -- TODO: 这里是根据表判断这个NPC是否是可战斗的NPC
+    if true then
+        invoke(
+            function()
+                wait(1)
+                local healthVal = world:CreateObject('IntValueObject', 'HealthVal', _npcObj)
+                local attackVal = world:CreateObject('IntValueObject', 'AttackVal', _npcObj)
+                local monsterVal = world:CreateObject('ObjRefValueObject', 'MonsterVal', _npcObj)
+                world:CreateObject('IntValueObject', 'BattleVal', _npcObj)
+                monsterVal.Value =
+                    world:CreateInstance(
+                    'Monster',
+                    'Monster' .. _npcInfo.Name,
+                    world,
+                    _npcObj.Position - _npcObj.Forward * 2
+                )
+                MoveMonster(_npcObj, monsterVal.Value)
+            end
+        )
+    end
+end
+
+-- 移动宠物
+function MoveMonster(_npcobj, _monster)
     invoke(
         function()
-            local _moveTime = 2
-            while true do
-                if _monster and _monster.Cube then
-                    local Tweener =
-                        Tween:TweenProperty(
-                        _monster.Cube,
-                        {LocalPosition = Vector3(0, 2.5, 0)},
-                        _moveTime,
-                        Enum.EaseCurve.Linear
-                    )
-                    Tweener:Play()
-                    wait(_moveTime)
-                    Tweener =
-                        Tween:TweenProperty(
-                        _monster.Cube,
-                        {LocalPosition = Vector3(0, 2, 0)},
-                        _moveTime,
-                        Enum.EaseCurve.Linear
-                    )
-                    Tweener:Play()
-                    wait(_moveTime)
-                end
+            local timeUp, timeDown = 3, 2
+
+            -- 插入一个随机值，让NPC的宠物错落有致的移动
+            wait(math.random() * timeUp)
+
+            -- 宠物向上
+            local twUp =
+                Tween:TweenProperty(
+                _monster.Cube,
+                {
+                    LocalPosition = Vector3(0, 2.5, 0)
+                },
+                timeUp,
+                Enum.EaseCurve.SinOut
+            )
+            -- 宠物向下
+            local twDown =
+                Tween:TweenProperty(
+                _monster.Cube,
+                {
+                    LocalPosition = Vector3(0, 2, 0)
+                },
+                timeDown,
+                Enum.EaseCurve.BackOut
+            )
+            while _monster and _monster.Cube do
+                twUp:Play()
+                wait(timeUp + .5)
+                twDown:Play()
+                wait(timeDown)
             end
         end
     )
