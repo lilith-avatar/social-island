675ad5a0158ab247bcc4ddb58bde64a0c333e0ce
lilith-avatar-richardzhou <64085306+lilith-avatar-richardzhou@users.noreply.github.com>
2021-01-06 18:57:11 +0800

    Update ['World']['Global']['Module']['Fsm_Module']['FsmBaseModule'].ModuleScript.lua
    完美的重构

--- Code/['World']['Global']['Module']['Fsm_Module']['FsmBaseModule'].ModuleScript.lua
+++ Code/['World']['Global']['Module']['Fsm_Module']['FsmBaseModule'].ModuleScript.lua
@@ -7,6 +7,8 @@ local FsmBase = class("FsmBase")
 function FsmBase:initialize()
     print("FsmBase:initialize()")
     self.states = {}
+    self.stateEnum = {}
+    self.stateTrigger = {}
     self.lastState = nil
     self.curState = nil
 end
@@ -14,6 +16,8 @@ end
 --向状态机添加状态
 function FsmBase:AddState(_state)
     self.states[_state.stateName] = _state
+    self.stateEnum[string.upper(_state.stateName)] = _state.stateName
+    self.stateTrigger[_state.stateName] = false
 end
 
 --初始化默认状态
@@ -40,15 +44,39 @@ function FsmBase:Switch(_stateName)
     self.curState:OnEnter()
 end
 
+--- 触发触发器
+function FsmBase:ContactTrigger(_stateName)
+    if self.curState.stateName ~= _stateName then
+        self.stateTrigger[_stateName] = true
+    end
+end
+
+--- 重置触发器
+function FsmBase:ResetTrigger()
+    for k, v in pairs(self.stateTrigger) do
+        v = false
+    end
+end
+
+--- 监听触发器
+function FsmBase:TriggerMonitor(_stateNameTable)
+    for k, v in pairs(_stateNameTable) do
+        if self.stateTrigger[v] then
+            self:Switch(v)
+        end
+    end
+end
+
 --绑定所有状态function
 function FsmBase:ConnectStateFunc(_statesT, _module)
     for k, v in pairs(_statesT) do
         if
-            _module[v.Name .. "StateOnEnterFunc"] and _module[v.Name .. "StateOnUpdateFunc"] and
+            _module[v.Name .. "StateOnEnterFunc"] and _Fmodule[v.Name .. "StateOnUpdateFunc"] and
                 _module[v.Name .. "StateOnLeaveFunc"]
          then
             local tempState = StateBase:new(v.Name, v.NextName, v.Dur)
             tempState.OnEnter = function()
+                self:ResetTrigger()
                 _module[v.Name .. "StateOnEnterFunc"]()
             end
             tempState.OnUpdate = function(dt)
