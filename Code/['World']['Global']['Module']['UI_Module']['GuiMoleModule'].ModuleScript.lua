e5ef3953d3cebf1d9de7dd86f7baa87daed7f04f
lilith-avatar-yenyuan <64084912+lilith-avatar-yenyuan@users.noreply.github.com>
2021-01-11 18:07:56 +0800

    给ChairUIMgr和MoleUIMgr改名

--- Code/['World']['Global']['Module']['UI_Module']['GuiMoleModule'].ModuleScript.lua
+++ Code/['World']['Global']['Module']['UI_Module']['GuiMoleModule'].ModuleScript.lua
@@ -1,18 +1,18 @@
----@module MoleUIMgr
+---@module GuiMole
 ---@copyright Lilith Games, Avatar Team
 ---@author Yen Yuan
-local MoleUIMgr, this = ModuleUtil.New('MoleUIMgr', ClientBase)
+local GuiMole, this = ModuleUtil.New('GuiMole', ClientBase)
 
 ---初始化函数
-function MoleUIMgr:Init()
-    print('[MoleUIMgr] Init()')
+function GuiMole:Init()
+    print('[GuiMole] Init()')
     this:NodeDef()
     this:DataInit()
     this:EventBind()
 end
 
 ---节点定义
-function MoleUIMgr:NodeDef()
+function GuiMole:NodeDef()
     this.contrlGui = localPlayer.Local.ControlGui
     this.gui = localPlayer.Local.MoleHitGui
     this.hitButton = this.gui.HitBtn
@@ -23,20 +23,20 @@ function MoleUIMgr:NodeDef()
 end
 
 ---数据初始化
-function MoleUIMgr:DataInit()
+function GuiMole:DataInit()
     this.isCooling = false
     this.coolTime = 1
     this.timer = 0
 end
 
-function MoleUIMgr:GameOver()
+function GuiMole:GameOver()
     this.gui:SetActive(false)
     this.contrlGui.UseBtn:SetActive(true)
     --this.hitButton:SetActive(false)
 end
 
 ---事件绑定
-function MoleUIMgr:EventBind()
+function GuiMole:EventBind()
     this.hitButton.OnClick:Connect(
         function()
             if this.isCooling then
@@ -48,40 +48,40 @@ function MoleUIMgr:EventBind()
     )
 end
 
-function MoleUIMgr:StartMoleEventHandler()
+function GuiMole:StartMoleEventHandler()
     this:StartGame()
     MoleGame:GameStart()
 end
 
-function MoleUIMgr:Hit()
+function GuiMole:Hit()
     NetUtil.Fire_S('PlayerHitEvent', localPlayer.UserId, MoleGame.rangeList)
 end
 
-function MoleUIMgr:StartGame()
+function GuiMole:StartGame()
     NetUtil.Fire_S('PlayerStartMoleHitEvent', localPlayer.UserId)
     this.gui:SetActive(true)
     this.hitButton:SetActive(true)
     this.contrlGui.UseBtn:SetActive(false)
 end
 
-function MoleUIMgr:UpdateScore(_score)
+function GuiMole:UpdateScore(_score)
     this.scoreText.Text = math.floor(tonumber(_score))
 end
 
-function MoleUIMgr:UpdateBoost(_boostNum)
+function GuiMole:UpdateBoost(_boostNum)
     this.boostText.Text = math.floor(tonumber(_boostNum))
 end
 
-function MoleUIMgr:UpdateTime(_time)
+function GuiMole:UpdateTime(_time)
     this.timeText.Text = math.floor(tonumber(_time))
 end
 
 ---强化过程表现
-function MoleUIMgr:Boosting()
+function GuiMole:Boosting()
 end
 
 ---Update函数
-function MoleUIMgr:Update(_dt)
+function GuiMole:Update(_dt)
     if this.isCooling then
         this.timer = this.timer + _dt
         this.hitMask.FillAmount = 1 - this.timer / this.coolTime
@@ -92,4 +92,4 @@ function MoleUIMgr:Update(_dt)
     end
 end
 
-return MoleUIMgr
+return GuiMole
