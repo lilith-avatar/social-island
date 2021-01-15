ec06c69a549c908327421ed0d0604713d833b8bf
lilith-avatar-richardzhou <64085306+lilith-avatar-richardzhou@users.noreply.github.com>
2021-01-15 17:12:06 +0800

    找回表

--- Code/['World']['Global']['Module']['Item_Module']['PlaceableItemModule'].ModuleScript.lua
+++ Code/['World']['Global']['Module']['Item_Module']['PlaceableItemModule'].ModuleScript.lua
@@ -1,12 +1,12 @@
 --- 武器基类
--- @module WeaponBase
+-- @module PlaceableItem
 -- @copyright Lilith Games, Avatar Team
 -- @author Dead Ratman
-local WeaponBase = class("WeaponBase", ItemBase)
+local PlaceableItem = class("PlaceableItem", ItemBase)
 
-function WeaponBase:initialize(_data)
+function PlaceableItem:initialize(_data)
     ItemBase.initialize(self, _data)
-    print("WeaponBase:initialize()")
+    print("PlaceableItem:initialize()")
     self.isUsable = true
     self.isEquipable = true
     self.weaponObj = nil
@@ -15,94 +15,23 @@ function WeaponBase:initialize(_data)
 end
 
 --放入背包
-function WeaponBase:PutIntoBag()
+function PlaceableItem:PutIntoBag()
 end
 
 --从背包里扔掉
-function WeaponBase:ThrowOutOfBag()
+function PlaceableItem:ThrowOutOfBag()
 end
 
 --使用
-function WeaponBase:Use()
+function PlaceableItem:Use()
     if self.useCT == 0 then
         ItemBase.Use(self)
-        self:Equip()
     end
-end
-
---装备
-function WeaponBase:Equip()
-    self.weaponObj =
-        world:CreateInstance(
-        self.config.ModelName,
-        self.config.ModelName .. "Instance",
-        self.config.ParentNode,
-        self.config.ParentNode.Positon + self.config.Offset,
-        self.config.ParentNode.Rotation + self.config.Angle
-    )
-    self:PlayIdleAnim()
-end
-
---取下装备
-function WeaponBase:Unequip()
-    NetUtil.Fire_C("FsmTriggerEvent", localPlayer, "Idle")
-end
-
---攻击
-function WeaponBase:Attack()
-    self.attackCT = self.config.AttackCD
-    self:PlayAttackAnim()
-end
-
---获取攻击数据
-function WeaponBase:GetAttackData()
-    return {
-        healthChange = self.config.HealthChange,
-        hitAddBuff = self.config.HitAddBuff,
-        hitRemoveBuff = self.config.HitRemoveBuff
-    }
-end
-
---播放使用音效
-function WeaponBase:PlayUseSound()
-    NetUtil.Fire_C("PlayEffectEvent", self.config.UseSoundID)
-end
-
---播放命中特效
-function WeaponBase:PlayHitEffect(_pos)
-    local effect =
-        world:CreateInstance(self.config.HitEffectName, self.config.HitEffectName .. "Instance", self.weaponObj, _pos)
-    invoke(
-        function()
-            effect:Destroy()
-        end,
-        1
-    )
-end
-
---播放命中音效
-function WeaponBase:PlayHitSound(_pos)
-    NetUtil.Fire_C("PlayEffectEvent", self.config.HitSoundID, _pos)
-end
-
---切换到待机动作
-function WeaponBase:PlayIdleAnim()
-    NetUtil.Fire_C("FsmTriggerEvent", localPlayer, self.config.Anim .. "Idle")
-end
-
---切换到攻击动作
-function WeaponBase:PlayAttackAnim()
-    NetUtil.Fire_C("FsmTriggerEvent", localPlayer, self.config.Anim .. "Attack1")
 end
 
 --CD消退
-function WeaponBase:CDRun(dt)
+function PlaceableItem:CDRun(dt)
     ItemBase.CDRun(self, dt)
-    if self.attackCT > 0 then
-        self.attackCT = self.attackCT - dt
-    elseif self.attackCT < 0 then
-        self.attackCT = 0
-    end
 end
 
-return WeaponBase
+return PlaceableItem
