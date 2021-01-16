e0503338dd1dc701d395e5856051137b261601da
lilith-avatar-richardzhou <64085306+lilith-avatar-richardzhou@users.noreply.github.com>
2021-01-16 18:58:34 +0800

    删除多余表格
    道具和Buff系统完成，等待对接PlayerData

--- Code/['World']['Global']['Module']['C_Module']['ItemMgrModule'].ModuleScript.lua
+++ Code/['World']['Global']['Module']['C_Module']['ItemMgrModule'].ModuleScript.lua
@@ -6,6 +6,8 @@
 
 local ItemMgr, this = ModuleUtil.New("ItemMgr", ClientBase)
 
+local instantiateItemFunc = {}
+
 function ItemMgr:Init()
     print("ItemMgr:Init")
     this:NodeRef()
@@ -23,6 +25,41 @@ end
 
 --节点事件绑定
 function ItemMgr:EventBind()
+end
+
+--实例化近战武器
+function ItemMgr:Instantiate1(_id)
+    return MeleeWeapon:new(Config.Item[_id], Config.MeleeWeapon[_id])
+end
+
+--实例化远程武器
+function ItemMgr:Instantiate2(_id)
+    return LongRangeWeapon:new(Config.Item[_id], Config.LongRangeWeapon[_id])
+end
+
+--实例化即时使用型道具
+function ItemMgr:Instantiate3(_id)
+    return UsableItem:new(Config.Item[_id], Config.UsableItem[_id])
+end
+
+--实例化放置型道具
+function ItemMgr:Instantiate4(_id)
+    return PlaceableItem:new(Config.Item[_id], Config.PlaceableItem[_id])
+end
+
+--实例化任务型道具
+function ItemMgr:Instantiate5(_id)
+    return TaskItem:new(Config.Item[_id], Config.TaskItem[_id])
+end
+
+--实例化奖励型道具
+function ItemMgr:Instantiate6(_id)
+    return RewardItem:new(Config.Item[_id], Config.RewardItem[_id])
+end
+
+--实例化物品
+function ItemMgr:InstantiateItem(_id)
+    return this["Instantiate" .. string.sub(tostring(_id), 1, 1)]
 end
 
 function ItemMgr:Update(dt, tt)
