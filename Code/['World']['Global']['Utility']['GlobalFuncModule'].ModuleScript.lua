802814b22a68efd3c58065f1ca8e9975f01d9fe2
Yuancheng Zhang <zhangyuancheng@lilith.com>
2021-01-28 02:59:30 +0800

    更新NPC空闲动画和气泡；将CheckHitObjIsPlayer函数移动到GlobalFunc模块中，删除ServerUtil模块

--- Code/['World']['Global']['Utility']['GlobalFuncModule'].ModuleScript.lua
+++ Code/['World']['Global']['Utility']['GlobalFuncModule'].ModuleScript.lua
@@ -23,4 +23,11 @@ function GlobalFunc.UploadLogs(_tableName, ...)
     end
 end
 
+-- 检查碰撞对象是否为NPC
+-- Server-side 一般用于服务器端
+function GlobalFunc.CheckHitObjIsPlayer(_hitObj)
+    return _hitObj and _hitObj.ClassName == 'PlayerInstance' and _hitObj.Avatar and
+        _hitObj.Avatar.ClassName == 'PlayerAvatarInstance'
+end
+
 return GlobalFunc
