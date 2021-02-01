fc94d17fa092d139c5c4815045df5492cce533fe
lilith-avatar-yenyuan <64084912+lilith-avatar-yenyuan@users.noreply.github.com>
2021-02-01 18:16:12 +0800

    更改一个guichair的历史遗留问题

--- Code/['World']['Global']['Module']['UI_Module']['GuiChairModule'].ModuleScript.lua
+++ Code/['World']['Global']['Module']['UI_Module']['GuiChairModule'].ModuleScript.lua
@@ -1,20 +1,20 @@
----@module ChairUIMgr
+---@module GuiChair
 ---@copyright Lilith Games, Avatar Team
 ---@author Yen Yuan
-local ChairUIMgr, this = ModuleUtil.New("ChairUIMgr", ClientBase)
+local GuiChair, this = ModuleUtil.New("GuiChair", ClientBase)
 
 local type = ""
 local chairId = 0
 
 ---初始化函数
-function ChairUIMgr:Init()
-    print("[ChairUIMgr] Init()")
+function GuiChair:Init()
+    print("[GuiChair] Init()")
     this:NodeDef()
     this:DataInit()
     this:EventBind()
 end
 
-function ChairUIMgr:DataInit()
+function GuiChair:DataInit()
     this.normalState = nil
     this.dirQte = nil
     this.startUpdate = false
@@ -22,7 +22,7 @@ function ChairUIMgr:DataInit()
     this.buttonKeepTime = 0
 end
 
-function ChairUIMgr:EventBind()
+function GuiChair:EventBind()
     for k, v in pairs(this.normalBtn) do
         v.OnClick:Connect(
             function()
@@ -45,7 +45,7 @@ function ChairUIMgr:EventBind()
     )]]
 end
 
-function ChairUIMgr:NodeDef()
+function GuiChair:NodeDef()
     this.sitBtn = localPlayer.Local.ControlGui.SitBtn
     this.gui = localPlayer.Local.ChairGui
     this.normalGui = this.gui.NormalPnl
@@ -65,19 +65,19 @@ function ChairUIMgr:NodeDef()
     this.qteTotalTime = this.QteGui.TimeTxt.NumTxt
 end
 
-function ChairUIMgr:ClickSitBtn(_type, _chairId)
+function GuiChair:ClickSitBtn(_type, _chairId)
     NetUtil.Fire_S("PlayerClickSitBtnEvent", localPlayer.UserId, _type, _chairId)
     this.sitBtn:SetActive(false)
 end
 
 
-function ChairUIMgr:InteractCEventHandler(_id)
+function GuiChair:InteractCEventHandler(_id)
     if _id == 10 then
         NetUtil.Fire_S("PlayerClickSitBtnEvent", localPlayer.UserId, type, chairId)
     end
 end
 
-function ChairUIMgr:ShowSitBtnEventHandler(_type, _chairId)
+function GuiChair:ShowSitBtnEventHandler(_type, _chairId)
     --[[this.sitBtn.OnClick:Clear()
     this.sitBtn.OnClick:Connect(
         function()
@@ -89,11 +89,11 @@ function ChairUIMgr:ShowSitBtnEventHandler(_type, _chairId)
     chairId = _chairId
 end
 
-function ChairUIMgr:HideSitBtnEventHandler()
+function GuiChair:HideSitBtnEventHandler()
     this.sitBtn:SetActive(false)
 end
 
-function ChairUIMgr:EnterNormal()
+function GuiChair:EnterNormal()
     this.startUpdate = false
     this.gui:SetActive(true)
     this.QteGui:SetActive(false)
@@ -103,13 +103,13 @@ function ChairUIMgr:EnterNormal()
     NetUtil.Fire_C("ChangeMiniGameUIEvent", localPlayer, 10)
 end
 
-function ChairUIMgr:EnterQte()
+function GuiChair:EnterQte()
     this.gui:SetActive(true)
     this.QteGui:SetActive(true)
     NetUtil.Fire_C("ChangeMiniGameUIEvent", localPlayer, 10)
 end
 
-function ChairUIMgr:NormalShake(_upOrDown)
+function GuiChair:NormalShake(_upOrDown)
     NetUtil.Fire_S("NormalShakeEvent", Chair.chair, _upOrDown)
     if _upOrDown == "up" then
         this.normalBtn.down:SetActive(true)
@@ -120,7 +120,7 @@ function ChairUIMgr:NormalShake(_upOrDown)
     end
 end
 
-function ChairUIMgr:NormalBack()
+function GuiChair:NormalBack()
     this.normalGui:SetActive(false)
     this.QteGui:SetActive(false)
     this.gui:SetActive(false)
@@ -131,7 +131,7 @@ function ChairUIMgr:NormalBack()
     NetUtil.Fire_C("ChangeMiniGameUIEvent", localPlayer)
 end
 
-function ChairUIMgr:GetQteForward(_dir, _speed)
+function GuiChair:GetQteForward(_dir, _speed)
     for _, v in pairs(this.qteBtn) do
         v:SetActive(false)
     end
@@ -144,7 +144,7 @@ function ChairUIMgr:GetQteForward(_dir, _speed)
     NetUtil.Fire_S("QteChairMoveEvent", _dir, _speed, Chair.chair)
 end
 
-function ChairUIMgr:QteButtonClick(_dir)
+function GuiChair:QteButtonClick(_dir)
     --判断是否正确按钮
     if _dir ~= this.dirQte then
         --把玩家甩出去
@@ -155,15 +155,15 @@ function ChairUIMgr:QteButtonClick(_dir)
     this.timer = 0
 end
 
-function ChairUIMgr:ShowQteButton(_keepTime)
+function GuiChair:ShowQteButton(_keepTime)
     this.buttonKeepTime = _keepTime
 end
 
-function ChairUIMgr:ChangeTotalTime(_total)
+function GuiChair:ChangeTotalTime(_total)
     this.qteTotalTime.Text = tostring(math.floor(_total))
 end
 
-function ChairUIMgr:Update(_dt)
+function GuiChair:Update(_dt)
     if this.startUpdate and this.buttonKeepTime ~= 0 then
         this.timer = this.timer + _dt
         if this.timer >= this.buttonKeepTime then
@@ -175,4 +175,4 @@ function ChairUIMgr:Update(_dt)
     end
 end
 
-return ChairUIMgr
+return GuiChair
