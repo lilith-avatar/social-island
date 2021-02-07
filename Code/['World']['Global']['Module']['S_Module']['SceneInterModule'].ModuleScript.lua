de159c3819ff34033de816c2beb6cffc6a179922
lilith-avatar-yenyuan <64084912+lilith-avatar-yenyuan@users.noreply.github.com>
2021-02-07 11:32:17 +0800

    草动

--- Code/['World']['Global']['Module']['S_Module']['SceneInterModule'].ModuleScript.lua
+++ Code/['World']['Global']['Module']['S_Module']['SceneInterModule'].ModuleScript.lua
@@ -3,31 +3,40 @@
 ---@author Yen Yuan
 local SceneInter, this = ModuleUtil.New("SceneInter", ServerBase)
 local folder = {
-    Grass = nil,
-    Bush = nil,
-    Tree = nil,
-    Stone = nil,
-    Fish = nil
+    Grass = world.Grass,
+    --Bush = nil,
+    --Tree = nil,
+    --Stone = nil,
+    --Fish = nil
 } --场景路径
 
 ---初始化函数
 function SceneInter:Init()
-    --this:CreateInterManager()
+    this:CreateInterManager()
 end
 
 function SceneInter:CreateInterManager()
     for k, v in pairs(folder) do
-        for _, n in pairs(v:GetChildren()) do
-            n.OnCollisionBegin:Connect(
-                function(_hitObject)
-                    this[k .. "Inter"](_hitObject, n, self)
-                end
-            )
+        for m, n in pairs(v:GetDescendants()) do
+            if not n:IsA("NodeObject") then
+                n.OnCollisionBegin:Connect(
+                    function(_hitObject)
+                        this[k .. "Inter"](self,_hitObject, n)
+                    end
+                )
+            end
         end
     end
 end
 
 function SceneInter:GrassInter(_hitObject, _Object)
+    if _hitObject.ClassName == "PlayerInstance" then
+        print(_hitObject.ClassName)
+        local tweener = Tween:ShakeProperty(_Object, {"Rotation"}, 0.15, 8)
+        tweener:Play()
+        tweener:WaitForComplete()
+        tweener = nil
+    end
 end
 
 return SceneInter
