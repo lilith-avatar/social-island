21a94868c27316ab12ea5d6eb7e74b988d829a03
lilith-avatar-yenyuan <64084912+lilith-avatar-yenyuan@users.noreply.github.com>
2021-02-24 16:48:16 +0800

    打地鼠服务端处理

--- Code/['World']['Global']['Module']['S_Module']['MoleHitModule'].ModuleScript.lua
+++ Code/['World']['Global']['Module']['S_Module']['MoleHitModule'].ModuleScript.lua
@@ -4,34 +4,32 @@
 local MoleHit, this = ModuleUtil.New("MoleHit", ServerBase)
 local totalWeights = 0
 
----得到总权重
-local function GetTotalWeights(_moleConfig)
-end
-
----随机权重排序
-local function RandomSortByWeights(_moleConfig)
-end
-
-local function TransformTable(_table)
+local function SelectPit(_pitList, _num)
 end
 
 ---初始化函数
 function MoleHit:Init()
     print("[MoleHit] Init()")
     this:DataInit()
-    this:PitListInit()
-    this:PoolInit()
+    this:RefreashMole("ufo")
+    this:RefreashMole("maze")
 end
 
 function MoleHit:DataInit()
-    this.hitNum = {
+    this.startUpdate = false
+    this.RefreshList = {}
+    this.pitList = {
+        ufo = {},
+        maze = {}
+    }
+    this.hitTime = {
         ufo = 0,
         maze = 0
     }
-    --TODO： 读表
+    -- 读表
     this.hitNum = {
-        ufo = 100,
-        maze = 100
+        ufo = Config.MoleGlobalConfig.UFOPitNum.Value,
+        maze = Config.MoleGlobalConfig.MazePitNum.Value
     }
 end
 
@@ -41,13 +39,11 @@ function MoleHit:PoolInit()
     end
 end
 
---选定坑位
-function MoleHit:PitListInit()
-    -- ufo坑位
-    -- maze坑位
-end
-
-function MoleHit:RefreashMole()
+function MoleHit:RefreashMole(_type)
+    -- 遍历对应坑位
+    for k, v in pairs(this.pitList[_type]) do
+        this.molePool[_type]:Create()
+    end
 end
 
 function MoleHit:InteractSEventHandler(_player, _gameId)
@@ -55,12 +51,47 @@ function MoleHit:InteractSEventHandler(_player, _gameId)
     end
 end
 
-local player
-function MoleHit:PlayerHitEventHandler(_uid, _mole)
+--- 玩家击中地鼠事件
+function MoleHit:PlayerHitEventHandler(_uid,_type, _pit)
+    this:HitMoleAction(_uid,_type, _pit)
+    -- 增加数量
+    this.hitTime[_type] = this.hitTime[_type] + 1
+    -- TODO： 广播事件
+    --NetUtil.Broadcast('',this.hitTime[_type])
+    -- 判断是否达到彩蛋条件
+    if this.hitTime[_type] >= this.hitNum[_type] then
+        this.startUpdate,this.hitTime[_type] = true,0
+        this.RefreshList[_type] = {
+            timer = 0
+        }
+        --开启对应彩蛋
+    end
+end
+
+function MoleHit:HitMoleAction(_uid,_type, _pit)
+    -- 打击表现
+    local effect = world:CreateInstance('HitMoleEffect','Effect',_pit,_pit.Position,_pit.Rotation)
+    invoke(function()
+        effect:Destroy()
+    end, 1)
+    -- 摧毁地鼠
+    this.pitList[_type][_pit]:Destroy()
 end
 
 function MoleHit:EnterMiniGameEventHandler(_player, _gameId)
     if _gameId == 2 then
+    end
+end
+
+function MoleHit:Update(dt)
+    if this.startUpdate then
+        for k, v in pairs(this.RefreshList) do
+            v.timer = v.timer + dt
+            if v.timer >= Config.MoleGlobalConfig.RefreshTime.Value then
+                this:RefreashMole(k)
+                this.RefreshList[k] = nil
+            end
+        end
     end
 end
 
