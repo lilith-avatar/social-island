ceed1adc1f55e34d2c73b3f45717e12fb14ef955
lilith-avatar-yenyuan <64084912+lilith-avatar-yenyuan@users.noreply.github.com>
2021-02-25 16:01:49 +0800

    判断金钱是否支付

--- Code/['World']['Global']['Module']['UI_Module']['GuiMoleModule'].ModuleScript.lua
+++ Code/['World']['Global']['Module']['UI_Module']['GuiMoleModule'].ModuleScript.lua
@@ -22,31 +22,41 @@ end
 
 ---事件绑定
 function GuiMole:EventBind()
-    this.cancel.OnClick:Connect(function()
-        NetUtil.Fire_C('ResetDefUIEvent',localPlayer)
-    end)
-    this.pay.OnClick:Connect(function()
-        -- TODO: 进行支付 
-        -- ! Test 现在没有进行判断
-        this:Pay()
-        NetUtil.Fire_C('ResetDefUIEvent',localPlayer)
-    end)
+    this.cancel.OnClick:Connect(
+        function()
+            NetUtil.Fire_C("ResetDefUIEvent", localPlayer)
+        end
+    )
+    this.pay.OnClick:Connect(
+        function()
+            -- 进行支付
+            this:Pay()
+            NetUtil.Fire_C("ResetDefUIEvent", localPlayer)
+        end
+    )
 end
 
 ---数据初始化
 function GuiMole:DataInit()
     this.curMoleType = nil
     this.curPit = nil
+    this.curPrice = 0
 end
 
 function GuiMole:Pay()
-    NetUtil.Fire_S('PlayerHitEvent',localPlayer.UserId,this.curMoleType,this.curPit)
+    if Data.Player.coin >= this.curPrice then
+        NetUtil.Fire_C("UpdateCoinEvent", localPlayer, -1 * this.curPrice)
+        NetUtil.Fire_S("PlayerHitEvent", localPlayer.UserId, this.curMoleType, this.curPit)
+    else
+        print('金钱不足')
+    end
 end
 
-function GuiMole:GetPriceEventHandler(_price,_type,_pit)
-    this.des.Text = string.format('需要支付 %s 来开启', _price)
+function GuiMole:GetPriceEventHandler(_price, _type, _pit)
+    this.des.Text = string.format("需要支付 %s 来开启", _price)
     this.curMoleType = _type
     this.curPit = _pit
+    this.curPrice = _price
 end
 
 function GuiMole:InteractCEventHandler(_gameId)
