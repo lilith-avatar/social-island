069d52b324f86c95a95ff2ccdb0cb1a27112808a
lilith-avatar-yenyuan <64084912+lilith-avatar-yenyuan@users.noreply.github.com>
2021-02-27 16:32:30 +0800

    新背包ui接入数据

--- Code/['World']['Global']['Module']['UI_Module']['GuiBagModule'].ModuleScript.lua
+++ Code/['World']['Global']['Module']['UI_Module']['GuiBagModule'].ModuleScript.lua
@@ -3,41 +3,11 @@
 ---@author Yen Yuan
 local GuiBag, this = ModuleUtil.New("GuiBag", ClientBase)
 
-local transTab, tmp
----对背包后端数据传过来的表进行转换
----@param _itemTable table
----@return table
-local function TransformItemTable(_itemTable)
-    transTab = {}
-
-    for k, v in pairs(_itemTable) do
-        for i = 1, v.count do
-            local data = {
-                id = k,
-                cd = 0
-            }
-            table.merge(data, v)
-            table.insert(transTab, data)
-        end
-    end
-    tmp =
-        table.sort(
-        transTab,
-        function(a, b)
-            if a and b then
-                return (a.id < b.id)
-            end
-        end
-    )
-    return transTab --! Only Test
-end
-
 ---初始化函数
 function GuiBag:Init()
     this:NodeDef()
     this:DataInit()
-    --this:EventBind()
-    this:SlotBind()
+    this:EventBind()
 end
 
 function GuiBag:NodeDef()
@@ -47,8 +17,6 @@ function GuiBag:NodeDef()
 
     --* Button------------
     this.useBtn = this.gui.UseBtn
-    this.nextBtn = this.gui.NextBtn
-    this.prevBtn = this.gui.PrevBtn
     this.closeBtn = this.gui.CloseImg.CloseBtn
 
     --* Text--------------
@@ -60,51 +28,54 @@ end
 function GuiBag:DataInit()
     this.slotItem = {}
     this.pageIndex = 1 -- 页面序号
-    this.maxPage = nil -- 最大页数
+    this.maxPage = 1 -- 最大页数
     this.selectIndex = nil
 
     --* 背包物品显示参数-------------
-    this.rowNum = 5
-    this.colNum = 3
-end
-
---单元格按键事件绑定
-function GuiBag:SlotBind()
-    for k,v in pairs(this.slotList) do
-        v.ItemImg.SelectBtn.OnClick:Connect(function()
-            this:SelectItem(k)
-        end)
-    end
+    this.pageSize = 15
 end
 
 function GuiBag:EventBind()
+    this.closeBtn.OnClick:Connect(
+        function()
+            this:HideBagUI()
+        end
+    )
     this.useBtn.OnClick:Connect(
         function()
             this:ClickUseBtn(this.selectIndex)
         end
     )
-    this.nextBtn.OnClick:Connect(
-        function()
-            this:ClickChangePage(this.pageIndex + 1)
+    --单元格按键事件绑定
+    for k, v in pairs(this.slotList) do
+        v.ItemImg.SelectBtn.OnClick:Connect(
+            function()
+                this:SelectItem(k)
+            end
+        )
+    end
+end
+
+function GuiBag:TransItemTable()
+    for k,v in pairs(Data.Player.bag) do
+        if v.count == 0 then
+            goto Continue
         end
-    )
-    this.prevBtn.OnClick:Connect(
-        function()
-            this:ClickPrePage(this.pageIndex - 1)
-        end
-    )
-    this.backBtn.OnClick:Connect(
-        function()
-            this:HideBagUI()
-        end
-    )
+        local data = {
+            num = v.count,
+            id = k,
+            cd = 0
+        }
+        table.insert(this.slotItem, data)
+        ::Continue::
+    end
 end
 
 function GuiBag:ShowBagUI()
     this:ClearSelect()
     this.root:SetActive(true)
     -- 转表
-    this.slotItem = TransformItemTable(Data.Player.bag)
+    this:TransItemTable()
     -- 显示物品
     this:ClickChangePage(1)
     -- 根据长度获取最大页数
@@ -116,64 +87,65 @@ function GuiBag:HideBagUI()
 end
 
 function GuiBag:ShowItemByIndex(_index, _itemId)
-    this.slotItem[_index].id = _itemId
-
-    -- 更换图片
-    this.slotList[_index].IconImg.Texture = ResourceManager.GetTexture("UI/ItemIcon/" .. Config.Item[_itemId].Icon)
-    this.slotList[_index].IconImg.Size = this.slotList[_index].MaskImg.Size
-    this.slotList[_index].IconImg:SetActive(_itemId and true or false)
-
-    -- 若存在cd,则将mask放入表中
-    if not this.cdMask[_itemId] then
-        this.cdMask[_itemId] = {}
+    if not _itemId then
+        this.slotList[_index]:SetActive(false)
+        this.slotList[_index].ItemID.Value = ''
+        return
     end
-    table.insert(this.cdMask[_itemId], this.slotList[_index].MaskImg)
-
-    -- 红点系统前端表现
-    -- if this.slotItem[_index].isNew and this.slotItem[_index] then
-    --     --消除红点
-    --     this.slotList[_index].RedDotImg:SetActive(true)
-    -- end
+    this.slotList[_index].ItemID.Value = _itemId
+    -- 更换图片
+    this.slotList[_index].ItemImg.IMGNormal.Texture =
+        ResourceManager.GetTexture("UI/ItemIcon/" .. Config.Item[_itemId].Icon)
+    -- 显示数量
+    this.slotList[_index].ItemNumBg.NumText.Text = Data.Player.bag[_itemId].count
+    this.slotList[_index].ItemImg.IMGNormal.Size = Vector2(128, 128)
+    this.slotList[_index]:SetActive(_itemId and true or false)
 end
 
 function GuiBag:ClickUseBtn(_index)
-    if not this.selectIndex or this.timer[this.slotItem[((this.pageIndex - 1) * this.rowNum * this.colNum) + _index].id] then
+    if not this.selectIndex then
         return
     end
-    local itemId = this.slotItem[((this.pageIndex - 1) * this.rowNum * this.colNum) + _index].id
+    local itemId = this.slotList[_index].ItemID.Value
     this.cdMask[itemId] = {this.slotList[_index].MaskImg}
-    -- 该cd物品进入 cd
-    this.timer[itemId] = 0
-    -- TODO: 使用物品
-    -- 物品消耗判定
+    -- 使用物品
     NetUtil.Fire_C("UseItemEvent", localPlayer, itemId)
-    if not this.slotItem[((this.pageIndex - 1) * this.rowNum * this.colNum) + _index].isConst then
-        table.remove(this.slotItem, ((this.pageIndex - 1) * this.rowNum * this.colNum) + _index)
-        --Data.Player.bag[itemId].count = Data.Player.bag[itemId].count - 1
-        NetUtil.Fire_C("RemoveItemEvent", localPlayer, itemId)
-    end
+    -- 物品消耗判定
+    this:ConsumeItem(_index)
     -- 重新展示当前页面物品信息
     this:ClickChangePage(this.pageIndex)
     -- 清除选择
     this:ClearSelect()
 end
 
+function GuiBag:ConsumeItem(_index)
+    this.slotItem[(this.pageIndex - 1) * this.pageSize + _index].num =
+        this.slotItem[(this.pageIndex - 1) * this.pageSize + _index].num - 1
+    if this.slotItem[(this.pageIndex - 1) * this.pageSize + _index].num <= 0 then
+        table.remove(this.slotItem, (this.pageIndex - 1) * this.pageSize + _index)
+    end
+end
+
 ---选中物品
 function GuiBag:SelectItem(_index)
-    if this.slotItem[_index] then
+    if this.slotList[_index].ItemID.Value then
         this:ClearSelect()
         this.selectIndex = _index
         -- 进行名字和描述的更换,并高亮该物品
-        this:ChangeNameAndDesc(this.slotItem[_index].ItemID.Value)
-        this.slotItem[_index].ItemImg.Chosen:SetActive(true)
-        --判断是否开启使用按钮
-        --this.useBtn:SetActive(true)
+        this:ChangeNameAndDesc(this.slotList[_index].ItemID.Value)
+        this.slotList[_index].ItemImg.Chosen:SetActive(true)
+    --判断是否开启使用按钮
+    --this.useBtn:SetActive(true)
     end
 end
 
 function GuiBag:ClearSelect()
+    --清除描述
+    this.nameTxt.Text = " "
+    this.descTxt.Text = " "
+    this.useBtn:SetActive(false)
     if this.selectIndex then
-        -- TODO: 清除高亮
+        this.slotList[this.selectIndex].ItemImg.Chosen:SetActive(false)
         this.slotList[this.selectIndex].Image = ResourceManager.GetTexture("UI/Btn_Left")
         this.selectIndex = nil
         --清除描述
@@ -184,25 +156,26 @@ function GuiBag:ClearSelect()
 end
 
 function GuiBag:ClickChangePage(_pageIndex)
-    --清除cdmask
-    this.cdMask = {}
     this:ClearSelect()
     this:ShowItemsByPageIndex(_pageIndex)
+    this:RefreshPageBar(_pageIndex)
+end
 
+function GuiBag:RefreshPageBar(_pageIndex)
     --页面数字显示
     this.pageTxt = tostring(math.floor(_pageIndex))
     --如果第一页则不显示上一页按钮
     if _pageIndex <= 1 then
-        this.prevBtn:SetActive(false)
+        --this.prevBtn:SetActive(false)
     end
     --如果最后一页不显示下一页按钮
     if _pageIndex == this.maxPage then
-        this.nextBtn:SetActive(false)
+        --this.nextBtn:SetActive(false)
     end
     --其他情况打开全部按钮
     if _pageIndex ~= 1 and _pageIndex ~= this.maxPage then
-        this.prevBtn:SetActive(true)
-        this.nextBtn:SetActive(true)
+        --this.prevBtn:SetActive(true)
+        --this.nextBtn:SetActive(true)
     end
 end
 
@@ -212,13 +185,12 @@ function GuiBag:ChangeNameAndDesc(_itemId)
 end
 
 function GuiBag:ShowItemsByPageIndex(_pageIndex)
-    for i = 1, this.rowNum * this.colNum do
-        if this.slotItem[(_pageIndex - 1) * this.colNum * this.rowNum + i] then
+    for i = 1, this.pageSize do
+        if this.slotItem[(_pageIndex - 1) * this.pageSize + i] then
             -- 显示当前页面物品
-            this:ShowItemByIndex(i, this.slotItem[(_pageIndex - 1) * this.colNum * this.rowNum + i].id)
+            this:ShowItemByIndex(i, this.slotItem[(_pageIndex - 1) * this.pageSize + i].id)
         else
-            this.slotList[i].MaskImg.FillAmount = 0
-            this.slotList[i].IconImg:SetActive(false)
+            this.slotList[i]:SetActive(false)
         end
     end
     this:GetMaxPageNum(#this.slotItem)
@@ -226,7 +198,7 @@ end
 
 ---更新最大页面数
 function GuiBag:GetMaxPageNum(_itemNum)
-    this.maxPage = math.ceil(_itemNum / (this.colNum * this.rowNum))
+    this.maxPage = math.ceil(_itemNum / (this.pageSize))
     if this.maxPage <= 0 then
         this.maxPage = 1
     end
