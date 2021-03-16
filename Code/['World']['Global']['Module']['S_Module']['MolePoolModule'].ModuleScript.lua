8ebbedb1dd3773f8908c89ac748f1f55bc4f6155
lilith-avatar-yenyuan <64084912+lilith-avatar-yenyuan@users.noreply.github.com>
2021-03-16 10:55:26 +0800

    打地鼠接入迷宫

--- Code/['World']['Global']['Module']['S_Module']['MolePoolModule'].ModuleScript.lua
+++ Code/['World']['Global']['Module']['S_Module']['MolePoolModule'].ModuleScript.lua
@@ -50,6 +50,10 @@ function MoleClass:DataReset(_moleId)
     self.type = Config.MoleConfig[_moleId].Type
 end
 
+function MoleClass:SetActive(_active)
+    self.model:SetActive(_active)
+end
+
 function MoleClass:Reset(_moleId, _parent)
     self:CreateModel(_moleId, nil, _parent)
 end
