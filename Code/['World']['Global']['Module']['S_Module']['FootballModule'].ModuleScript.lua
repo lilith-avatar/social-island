50142352f736246e647c6ea042cba9065fe48841
lilith-avatar-yenyuan <64084912+lilith-avatar-yenyuan@users.noreply.github.com>
2021-03-19 11:24:53 +0800

    足球边界逻辑+ smap

--- Code/['World']['Global']['Module']['S_Module']['FootballModule'].ModuleScript.lua
+++ Code/['World']['Global']['Module']['S_Module']['FootballModule'].ModuleScript.lua
@@ -1,7 +1,7 @@
 ---@module Football
 ---@copyright Lilith Games, Avatar Team
 ---@author Yen Yuan
-local Football,this = ModuleUtil.New('Football',ServerBase)
+local Football, this = ModuleUtil.New("Football", ServerBase)
 
 ---初始化函数
 function Football:Init()
@@ -12,32 +12,52 @@ end
 function Football:DataInit()
     this.ball = world.Football.Ball
     this.goal = world.Football.Goal
+    this.border = world.Football.Border
     this.ballOriPos = this.ball.Position
 end
 
 function Football:EventBind()
-    for k,v in pairs(this.goal:GetChildren()) do
+    for k, v in pairs(this.goal:GetChildren()) do
         v.OnCollisionBegin:Connect(
             function(_hitObject)
-                if _hitObject.Name == 'Ball' and _hitObject.ClassName == 'Sphere' then
+                if _hitObject.Name == "Ball" and _hitObject.ClassName == "Sphere" then
                     this:FootballGoal(v)
                 end
             end
         )
     end
+    this.border.OnCollisionEnd:Connect(
+        function(_hitObject)
+            if _hitObject and _hitObject.Name == "Ball" and _hitObject.ClassName == "Sphere" then
+                this.ball.RefreshFx:SetActive(true)
+                wait(1)
+                this.ball.RefreshFx:SetActive(false)
+                this:FootballReset()
+            end
+        end
+    )
 end
 
 function Football:FootballGoal(_goal)
     this.ball.IsStatic = true
     _goal.GoalEffect:SetActive(true)
-    invoke(function()
-        _goal.GoalEffect:SetActive(false)
-        this.ball.Position = this.ballOriPos
-        this.ball.RefreshFx:SetActive(true)
-        this.ball.IsStatic = false
-        wait(1)
-        this.ball.RefreshFx:SetActive(false)
-    end, 1)
+    invoke(
+        function()
+            _goal.GoalEffect:SetActive(false)
+            this:FootballReset()
+        end,
+        1
+    )
+end
+
+function Football:FootballReset()
+    this.ball.IsStatic = true
+    this.ball.Position = this.ballOriPos
+    this.ball.LinearVelocity = Vector3.Zero
+    this.ball.RefreshFx:SetActive(true)
+    wait(1)
+    this.ball.IsStatic = false
+    this.ball.RefreshFx:SetActive(false)
 end
 
 return Football
