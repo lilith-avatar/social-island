ee8ec661b4212f8648f9114348647e8821930b13
lilith-avatar-yenyuan <64084912+lilith-avatar-yenyuan@users.noreply.github.com>
2021-03-21 17:05:01 +0800

    timesky配置表和时间流逝改变天光

--- Code/['World']['Global']['Module']['S_Module']['SceneTimeModule'].ModuleScript.lua
+++ Code/['World']['Global']['Module']['S_Module']['SceneTimeModule'].ModuleScript.lua
@@ -7,22 +7,24 @@
 ---? 其他的读表即可
 local SceneTime, this = ModuleUtil.New("SceneTime", ServerBase)
 local ModeEnum = {
-    Instant = 'Instant', -- 每个小时更改一次 
-    RealTime = 'RealTime' -- 随时间实时更改（大概？）
+    Instant = "Instant", -- 每个小时更改一次
+    RealTime = "RealTime" -- 随时间实时更改（大概？）
 }
 
-local TimeMode = ModeEnum.Instant --!可更改
+local TimeMode = ModeEnum.RealTime --!可更改
 
 ---初始化函数
 function SceneTime:Init()
     this:DataInit()
     this:NodeDef()
+    print(this.sky.Style)
 end
 
 function SceneTime:DataInit()
     this.timer = 0
     this.clock = 0 -- 当前游戏内时间
     this.timeSpeed = 5 -- 几秒1个小时
+    this.tweener = nil
 end
 
 function SceneTime:NodeDef()
@@ -31,13 +33,46 @@ end
 
 ---同步天空盒与表的数据
 function SceneTime:SycnSkyData()
-    this[TimeMode..'SycnSkyData'](self)
+    this[TimeMode .. "SycnSkyData"](self)
 end
 
 function SceneTime:InstantSycnSkyData()
+    for k, v in pairs(Config.TimeSkySetting[this.clock]) do
+        this.sky[k] = v
+    end
 end
 
 function SceneTime:RealTimeSycnSkyData()
+    local data, configData = {}, Config.TimeSkySetting[this.clock]
+    if this.tweener then
+        this.tweener:Pause()
+        this.tweener = nil
+    end
+    for k, v in pairs(Config.TimeSkySetting[this.clock - 1]) do
+        this.sky[k] = v
+    end
+    data = {
+        ClockTime = configData.ClockTime,
+        Brightness = configData.Brightness,
+        Latitude = configData.Latitude,
+        SunAngular = configData.SunAngular,
+        ShadowDistance = configData.ShadowDistance,
+        ShadowIntensity = configData.ShadowIntensity,
+        Ambient = configData.Ambient,
+        SunColor = configData.SunColor,
+        EquatorColor = configData.EquatorColor,
+        GroundColor = configData.GroundColor,
+        SunIntensity = configData.SunIntensity,
+        SkyboxIntensity = configData.SkyboxIntensity,
+        FogColor = configData.FogColor,
+        FogStart = configData.FogStart,
+        FogEnd = configData.FogEnd,
+        FogColor = configData.FogColor,
+        FogHeightFadeStart = configData.FogHeightFadeStart,
+        FogHeightFadeEnd = configData.FogHeightFadeEnd
+    }
+    local tweener = Tween:TweenProperty(this.sky, data, this.timeSpeed, 1)
+    tweener:Play()
 end
 
 ---Update函数
@@ -56,7 +91,7 @@ function SceneTime:Update(dt)
 end
 
 ---@param _clock number
-function SceneTime:SycnSkyDataSEventHandler(_clock)
+function SceneTime:SycnTimeSEventHandler(_clock)
     print(string.format("当前时间 %s 点", math.floor(_clock))) --! 上线删除
 end
 
