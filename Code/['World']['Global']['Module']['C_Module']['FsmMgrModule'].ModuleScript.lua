b0c8336ebee3619ed47c11c3af334e908e1a0876
lilith-avatar-richardzhou <64085306+lilith-avatar-richardzhou@users.noreply.github.com>
2021-03-22 12:00:46 +0800

    Update ['World']['Global']['Module']['C_Module']['FsmMgrModule'].ModuleScript.lua

--- Code/['World']['Global']['Module']['C_Module']['FsmMgrModule'].ModuleScript.lua
+++ Code/['World']['Global']['Module']['C_Module']['FsmMgrModule'].ModuleScript.lua
@@ -60,7 +60,7 @@ end
 
 function FsmMgr:Update(dt)
     --this.playerActFsm:Update(dt)
-    print(this.playerActFsm.curState.stateName)
+    --print(this.playerActFsm.curState.stateName)
     --print(this.playerActFsm.stateTrigger.BowAttack)
 end
 
