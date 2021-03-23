2a04d07a3f0240061a5667430e66d4667caf2cab
lilith-avatar-yenyuan <64084912+lilith-avatar-yenyuan@users.noreply.github.com>
2021-03-23 17:13:55 +0800

    做饭的基本前端逻辑

--- Code/['World']['Global']['Module']['UI_Module']['GuiCookModule'].ModuleScript.lua
+++ Code/['World']['Global']['Module']['UI_Module']['GuiCookModule'].ModuleScript.lua
@@ -1,13 +1,17 @@
 ---@module GuiCook
 ---@copyright Lilith Games, Avatar Team
 ---@author Yen Yuan
-local GuiCook,this = ModuleUtil.New('GuiCook',ClientBase)
+local GuiCook, this = ModuleUtil.New("GuiCook", ClientBase)
 
 ---初始化函数
 function GuiCook:Init()
     this:NodeDef()
     this:DataInit()
     this:EventBind()
+end
+
+function GuiCook:Test()
+    NetUtil.Fire_C("GetItemEvent", localPlayer, 6043)
 end
 
 function GuiCook:DataInit()
@@ -35,6 +39,8 @@ function GuiCook:NodeDef()
     this.closeBtn = this.gui.CloseImg.CloseBtn -- 关闭按钮
     this.prevBtn = this.gui.DragPanel.PreBtn -- 上一页按钮
     this.nextBtn = this.gui.DragPanel.NextBtn -- 下一页按钮
+    --* Text--------------------
+    this.pageTxt = this.gui.DragPanel.PageText
 end
 
 function GuiCook:EventBind()
@@ -46,35 +52,46 @@ function GuiCook:EventBind()
             end
         )
     end
-    for k,v in ipairs(this.MaterialSlot) do
-        v.ItemImg.SelectBtn.OnClick:Connect(function()
-            this:CancelMaterial(k)
-        end)
+    for k, v in ipairs(this.MaterialSlot) do
+        v.ItemImg.SelectBtn.OnClick:Connect(
+            function()
+                this:CancelMaterial(k)
+            end
+        )
     end
-    this.cookBtn.OnClick:Connect(function()
-        this:StartCook()
-    end)
+    this.cookBtn.OnClick:Connect(
+        function()
+            this:StartCook()
+        end
+    )
+    this.closeBtn.OnClick:Connect(
+        function()
+            this:HideGui()
+        end
+    )
 end
 
 function GuiCook:TransItemTable()
     --先清空表
-    this.slotItem = {}
+    this.BagMaterial = {}
     for k, v in pairs(Data.Player.bag) do
         if v.count > 0 and Config.RewardItem[k] then
-            local data = {
-                num = v.count,
-                id = k,
-                cd = 0
-            }
-            table.insert(this.BagMaterial, data)
+            for i = 1, v.count do
+                local data = {
+                    id = k
+                }
+                table.insert(this.BagMaterial, data)
+            end
         end
     end
 end
 
 function GuiCook:ShowUI()
     this:ClearAllMaterial()
+    this:TransItemTable()
     this.root:SetActive(true)
     this.gui:SetActive(true)
+    this:ClickChangePage(1)
 end
 
 function GuiCook:InteractCEventHandler(_gameId)
@@ -85,10 +102,12 @@ end
 
 function GuiCook:LeaveInteractCEventHandler(_gameId)
     if _gameId == 99 then
+        this:HideGui()
     end
 end
 
 function GuiCook:HideGui()
+    this.root:SetActive(false)
 end
 
 function GuiCook:StartCook()
@@ -101,26 +120,107 @@ function GuiCook:CancelMaterial(_index)
     if not this.UsingMaterial[_index] then
         return
     end
-    --this.MaterialSlot
+    table.insert(this.BagMaterial, this.UsingMaterial[_index])
     table.remove(this.UsingMaterial, _index)
     if #this.UsingMaterial == 0 or not this.UsingMaterial then
         this.cookBtn.Locked:SetActive(true)
     end
+    table.sort(
+        this.BagMaterial,
+        function(v1, v2)
+            return v1.id < v2.id
+        end
+    )
     this:ShowMaterialIcon()
+    this:ClickChangePage(this.pageIndex)
 end
 
 function GuiCook:ChooseMaterial(_index)
+    if #this.UsingMaterial >= 3 then
+        return
+    end
     this.cookBtn.Locked:SetActive(false)
+    table.insert(this.UsingMaterial, {id = this.slotList[_index].ItemID.Value})
+    table.remove(this.BagMaterial, _index + (this.pageIndex - 1) * this.pageSize)
+    this:ClickChangePage(this.pageIndex)
+    this:ShowMaterialIcon()
 end
 
 function GuiCook:ClearAllMaterial()
     this.UsingMaterial = {}
     this:ShowMaterialIcon()
+    this.cookBtn.Locked:SetActive(true)
 end
 
 function GuiCook:ShowMaterialIcon()
-    for k,v in ipairs(this.MaterialSlot) do
-        v.ItemImg.IMGNormal:SetActive(false)
+    for k, v in ipairs(this.MaterialSlot) do
+        if this.UsingMaterial[k] then
+            v.ItemImg.IMGNormal.Texture =
+                ResourceManager.GetTexture("UI/ItemIcon/" .. Config.Item[this.UsingMaterial[k].id].Icon)
+            v.ItemImg.IMGNormal.Size = Vector2(128, 128)
+            v.ItemImg:SetActive(true)
+        else
+            v.ItemImg:SetActive(false)
+        end
+    end
+end
+
+function GuiCook:ClickChangePage(_pageIndex)
+    this:ShowItemsByPageIndex(_pageIndex)
+    this:RefreshPageBar(_pageIndex)
+end
+
+function GuiCook:ShowItemsByPageIndex(_pageIndex)
+    for i = 1, this.pageSize do
+        if this.BagMaterial[(_pageIndex - 1) * this.pageSize + i] then
+            -- 显示当前页面物品
+            this:ShowItemByIndex(i, this.BagMaterial[(_pageIndex - 1) * this.pageSize + i].id)
+        else
+            this.slotList[i]:SetActive(false)
+        end
+    end
+    this:GetMaxPageNum(#this.BagMaterial)
+end
+
+function GuiCook:ShowItemByIndex(_index, _itemId)
+    if not _itemId then
+        this.slotList[_index]:SetActive(false)
+        this.slotList[_index].ItemID.Value = ""
+        return
+    end
+    this.slotList[_index].ItemID.Value = _itemId
+    -- 更换图片
+    this.slotList[_index].ItemImg.IMGNormal.Texture =
+        ResourceManager.GetTexture("UI/ItemIcon/" .. Config.Item[_itemId].Icon)
+    -- 显示数量
+    this.slotList[_index].ItemImg.IMGNormal.Size = Vector2(128, 128)
+    this.slotList[_index]:SetActive(_itemId and true or false)
+end
+
+---更新最大页面数
+function GuiCook:GetMaxPageNum(_itemNum)
+    this.maxPage = math.ceil(_itemNum / (this.pageSize))
+    if this.maxPage <= 0 then
+        this.maxPage = 1
+    end
+end
+
+function GuiCook:RefreshPageBar(_pageIndex)
+    this.pageIndex = _pageIndex
+    --页面数字显示
+    this.pageTxt.Text = tostring(math.floor(_pageIndex))
+    --如果第一页则不显示上一页按钮
+    if _pageIndex <= 1 then
+        this.prevBtn:SetActive(false)
+    end
+    --如果最后一页不显示下一页按钮
+    if _pageIndex == this.maxPage then
+        this.nextBtn:SetActive(false)
+    end
+    --其他情况打开全部按钮
+    if _pageIndex ~= 1 and _pageIndex ~= this.maxPage then
+        this.prevBtn:SetActive(true)
+        this.nextBtn:SetActive(true)
     end
 end
 
