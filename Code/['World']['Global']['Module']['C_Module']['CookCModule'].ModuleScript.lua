277cb638189dfdd3dd768905c56c532c4438bc8a
lilith-avatar-yenyuan <64084912+lilith-avatar-yenyuan@users.noreply.github.com>
2021-03-25 11:03:02 +0800

    菜品匹配机制

--- Code/['World']['Global']['Module']['C_Module']['CookCModule'].ModuleScript.lua
+++ Code/['World']['Global']['Module']['C_Module']['CookCModule'].ModuleScript.lua
@@ -1,7 +1,7 @@
 ---@module CookC
 ---@copyright Lilith Games, Avatar Team
 ---@author Yen Yuan
-local CookC,this = ModuleUtil.New('CookC',ClientBase)
+local CookC, this = ModuleUtil.New("CookC", ClientBase)
 
 ---初始化函数
 function CookC:Init()
@@ -12,14 +12,22 @@ end
 function CookC:DataInit()
 end
 
----Update函数
-function CookC:Update()
-end
-
 --根据食材处理最后做出的菜
 function CookC:PlayerCookEventHandler(_materialList)
-    -- for k,v in pairs() do
-    -- end
+    for k, v in pairs() do
+        if this:JudgeMaterialInMenu(_materialList, v) then
+            NetUtil.Fire_C("", localPlayer, v.ID)
+        end
+    end
+end
+
+function CookC:JudgeMaterialInMenu(_materialList, _menu)
+    for k, v in pairs(_materialList) do
+        if table.indexof(_menu, v, 1) == 0 then
+            return false
+        end
+    end
+    return true
 end
 
 function CookC:EventBind()
