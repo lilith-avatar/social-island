d759289d7830fcdcb0ed49bf218be8ca3148dcf9
Yuancheng Zhang <old.far.hot@gmail.com>
2021-05-07 19:18:59 +0800

    数据埋点GAME_ID初始化统一

--- Code/['World']['Global']['Module']['S_Module']['OtherModule'].ModuleScript.lua
+++ Code/['World']['Global']['Module']['S_Module']['OtherModule'].ModuleScript.lua
@@ -2,13 +2,12 @@
 --- @module Other
 --- @copyright Lilith Games, Avatar Team
 --- @author Yuancheng Zhang, Dead Ratman
-local Other, this = ModuleUtil.New("Other", ServerBase)
+local Other, this = ModuleUtil.New('Other', ServerBase)
 
 --! 初始化
 
 function Other:Init()
-    print("[Other] Init()")
-    CloudLogUtil.Init('Z1002')
+    print('[Other] Init()')
     SoundUtil.Init(Config.Sound)
     SoundUtil.InitAudioSource()
 end
@@ -17,14 +16,14 @@ end
 
 -- 玩家受伤事件
 function Other:SPlayerHitEventHandler(_attackPlayer, _hitPlayer, _data)
-    if _hitPlayer.Avatar.ClassName == "PlayerAvatarInstance" then
-        NetUtil.Fire_C("CPlayerHitEvent", _hitPlayer, _data)
+    if _hitPlayer.Avatar.ClassName == 'PlayerAvatarInstance' then
+        NetUtil.Fire_C('CPlayerHitEvent', _hitPlayer, _data)
     end
 end
 
 -- 播放全局音效
 function Other:SPlayEffectEventHandler(_id, _pos, _playerIndex)
-    NetUtil.Broadcast("PlayEffectEvent", _id, _pos, _playerIndex)
+    NetUtil.Broadcast('PlayEffectEvent', _id, _pos, _playerIndex)
 end
 
 return Other
